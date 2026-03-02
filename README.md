3fa5012541d3c9d48efab3f60da8782e9e86f92e
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-01 21:19:48 -0700

    Added initial structure.
    Added pseudocode for code.js, added some initial settings for the body and updated the README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # espConjDrill
+
 An interactive website that lets you practice conjugating various Spanish verbs.
+
+The idea is to use my new JS skills, especially in DOM manipulation and string methods, to solve a problem I'm currrently facing: I want to practice conjugating verbs in Spanish correctly. But I don't want to just sit down with a text book. I would rather have a program I can use to check my answers immediately.
---
+++ code.js
@@ -0,0 +1,11 @@
+//Ok I imagine the code is gonna go something like this:
+
+//ask for word
+//detect word type by chopping off final two letters of verb (make sure it is an infiinitive)
+
+//check for irregular verb (best to keep chopped version of verb as a separate var from the const, which will be the infinitive)
+
+//generate correct answers once verb type has been determined
+
+//generate fields for inputting answers
+//check answer function onclick
