f3b07b63e870036f4049c13bae73f2004231c891
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-03 22:15:23 -0700

    Added infinitive check.
    Script now checks if the entered text is an infinitive. Needs to be put in a function to run repetitively.

--- code.js
+++ code.js
@@ -1,11 +1,22 @@
 //Ok I imagine the code is gonna go something like this:
 
 /* ask for word */
-const infinitive = prompt("Enter an infinitive:", ""); //replace this with a DOM thingy down the line please.
-console.log(infinitive);
+let inputVerb = prompt("Enter an infinitive:", ""); //replace this with a DOM thingy down the line please.
+console.log(inputVerb);
 /*detect word type by chopping off final two letters of verb (make sure it is an infiinitive)*/
-let verbType = infinitive.slice(-2);
+let verbType = inputVerb.slice(-2);
+if (!(verbType == "er" || verbType == "ar" || verbType == "ir")) {
+  //check for actual infinitive. I can already see the utility of function-ing all of ths to re-run easy.
+  verbType = "notInfinitive";
+}
 console.log(verbType);
+verbType === "notInfinitive"
+  ? (inputVerb = prompt(
+      "Enter an infinitive(MUST end in 'er', 'ir', or 'ar'):",
+      "",
+    ))
+  : undefined;
+
 /*check for irregular verb (best to keep chopped version of verb as a separate var from the const, which will be the infinitive)*/
 if (verbType == "er") {
   if (infinitive == "atender" || infinitive == "hacer") {
